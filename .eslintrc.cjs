456b91f95839adec30afcf373f7be798b636a5a6
mrartur0074 <smiteartur01@mail.ru>
2023-11-26 18:47:09 +0600

    init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Используйте эту строку вместо 'prettier/@typescript-eslint'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ["react-refresh", "@typescript-eslint", "react", "prettier", "react-hooks"],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    camelcase: 'error',
+    'spaced-comment': 'error',
+    'no-duplicate-imports': 'error',
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/display-name': 'off',
+    'prefer-const': 'off',
+    'react/prop-types': 0,
+  },
+}
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+import Checker from 'vite-plugin-checker'
+
+export default defineConfig({
+  plugins: [react(), eslint(), Checker({typescript: true, overlay: true})],
+  server: {
+    port: 3000
+  }
+})
